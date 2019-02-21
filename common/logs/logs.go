9fbd7d061c96601fa17dd2a8b97d67e6cfaf8872
zc <zc2638@qq.com>
2019-02-21 14:55:43 +0800

    fix

--- common/logs/logs.go
+++ common/logs/logs.go
@@ -3,76 +3,10 @@ package logs
 import (
 	"fmt"
 	"github.com/zctod/tool/common/utils"
-	"io"
 	"log"
 	"os"
 	"strings"
-	"time"
 )
-
-const (
-	LOG_LEVEL_NORMAL = "normal"
-	LOG_LEVEL_INFO   = "info"
-	LOG_LEVEL_WARN   = "warn"
-	LOG_LEVEL_ERROR  = "error"
-)
-
-var dirPath = "logs/"
-var path = time.Now().Format("20060102") + ".log"
-
-var w io.Writer
-var (
-	normalLog = log.New(w, "", log.Ldate|log.Ltime|log.Lshortfile)
-	infoLog   = log.New(w, "[INFO]", log.Ldate|log.Ltime|log.Lshortfile)
-	warnLog   = log.New(w, "[WARN]", log.Ldate|log.Ltime|log.Lshortfile)
-	errorLog  = log.New(w, "[ERROR]", log.Ldate|log.Ltime|log.Lshortfile)
-)
-var (
-	normalPath = dirPath + path
-	infoPath   = dirPath + "info_" + path
-	warnPath   = dirPath + "warn_" + path
-	errorPath  = dirPath + "error_" + path
-
-	normalPathOrigin = ""
-	infoPathOrigin   = ""
-	warnPathOrigin   = ""
-	errorPathOrigin  = ""
-)
-var (
-	Normal = SetPath(normalLog, normalPath, LOG_LEVEL_NORMAL)
-	Info   = SetPath(infoLog, infoPath, LOG_LEVEL_INFO)
-	Warn   = SetPath(warnLog, warnPath, LOG_LEVEL_WARN)
-	Error  = SetPath(errorLog, errorPath, LOG_LEVEL_ERROR)
-)
-
-func SetPath(l *log.Logger, path string, levelType string) *log.Logger {
-	switch levelType {
-	case LOG_LEVEL_NORMAL:
-		if normalPath == normalPathOrigin {
-			return l
-		}
-		break;
-	case LOG_LEVEL_INFO:
-		if infoPath == infoPathOrigin {
-			return l
-		}
-		break;
-	case LOG_LEVEL_WARN:
-		if warnPath == warnPathOrigin {
-			return l
-		}
-		break;
-	case LOG_LEVEL_ERROR:
-		if errorPath == errorPathOrigin {
-			return l
-		}
-		break;
-	default:
-		break;
-	}
-	l.SetOutput(ReadPath(path))
-	return l
-}
 
 func CreateLog(path string) *log.Logger {
 	return log.New(ReadPath(path), "", log.Ldate|log.Ltime|log.Lshortfile)
