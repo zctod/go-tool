bfd2ddb9edb2e2fe56a9063482dbacd666843cdc
zc <zc2638@qq.com>
2019-03-20 14:54:55 +0800

    comment

--- config/configs.go
+++ config/configs.go
@@ -2,91 +2,19 @@
 package config
 
 import (
-	"errors"
-	"fmt"
-	"github.com/zctod/tool/common/utils"
-	"io/ioutil"
-	"os"
-	"reflect"
-	"regexp"
-	"strings"
+	"github.com/zctod/tool/common/util_confd"
 )
 
 // 写入配置项
+// Deprecated: please use package common/util_confd.
 func writeConfig(config interface{}, path string) error {
 
-	file, err := utils.CreateFile(path)
-	defer file.Close()
-	if err != nil {
-		return err
-	}
-	t := reflect.TypeOf(config).Elem()
-	for i := 0; i < t.NumField(); i++ {
-		var defVal, ctVal string
-		field := utils.CamelToUnderline(t.Field(i).Name)
-		tag := t.Field(i).Tag.Get("config")
-		if tag != "" {
-			tagArr := strings.Split(tag, ";")
-			for _, v := range tagArr {
-				if v != "" {
-					sts, err := regexp.MatchString(`^\s*default:`, v)
-					if err != nil {
-						fmt.Println(err)
-					}
-					if sts == true {
-						defValArr := strings.Split(v, "default:")
-						defVal = defValArr[1]
-						continue
-					}
-					sts, err = regexp.MatchString(`^\s*comment:`, v)
-					if err != nil {
-						fmt.Println(err)
-					}
-					if sts == true {
-						ctValArr := strings.Split(v, "comment:")
-						ctVal = "  // " + ctValArr[1]
-						continue
-					}
-				}
-			}
-		}
-		_, err := file.WriteString(field + "=" + defVal + ctVal + "\n")
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-	return nil
+	return util_confd.WriteConfig(config, path)
 }
 
 // 初始化配置
+// Deprecated: please use package common/util_confd.
 func InitConfig(config interface{}, path string) error {
 
-	body, err := ioutil.ReadFile(path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			err = writeConfig(config, path)
-			if err == nil {
-				return errors.New("env is not exist, automatically generated and filled in")
-			}
-		}
-		return err
-	}
-
-	c := reflect.ValueOf(config).Elem()
-
-	arr := strings.Split(string(body), "\n")
-	for _, v := range arr {
-		r, _ := regexp.Compile(`\s+//[\s\S]*$|^//[\s\S]*$`)
-		v = r.ReplaceAllString(v, "")
-
-		v = strings.Replace(v, " ", "", -1)
-		strArr := strings.Split(v, "=")
-		if len(strArr) != 2 {
-			continue
-		}
-		value := reflect.ValueOf(strArr[1])
-		field := utils.UnderlineToCamel(strArr[0])
-		c.FieldByName(field).Set(value)
-	}
-	return nil
+	return util_confd.InitConfig(config, path)
 }
